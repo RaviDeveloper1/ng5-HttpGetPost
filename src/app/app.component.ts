0de61dd7d3e19841d32c385ecbc5d25879c1872f
RaviDeveloper1 <aravi.us80@gmail.com>
2018-02-23 22:15:56 -0600

    added get method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  data: any;
+  constructor(private http: HttpClient ) {
+
+  }
+
+  ngOnInit(): void {
+    this.http.get('https://api.github.com/users/seeschweiler').subscribe(
+      (data: any) => {
+      console.log('User Login:' + data.login);
+      console.log('Bio:' + data.bio);
+      console.log('Company:' + data.company);
+      },
+      err => {
+        console.log('Error occured');
+      }
+    );
+  }
 }
+
